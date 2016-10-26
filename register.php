386f88ad051c18993fa88c6e99c1d81ee28f9feb
kerlux <kerlux@gmail.com>
2016-10-26 15:01:08 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://kertry1.herokuapp.com/execute.php';
+$BOT_TOKEN = '292400786:AAEChnhpLUtYhOuFn2b4FOPkWbMpqiUqFpA';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
