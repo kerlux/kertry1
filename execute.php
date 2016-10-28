3a155235f814ee3450a6441419c093628b479b44
kerlux <kerlux@gmail.com>
2016-10-28 11:34:45 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -12,6 +12,12 @@ $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $text = strtolower($text);
 
+if($text!="pizza")
+{
+	$response = "risposta 1";
+}
+else{
+
   $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
   $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("1.jpg")), 'caption' => $text);
   $ch = curl_init(); 
@@ -39,3 +45,4 @@ $text = strtolower($text);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
   // read curl response
   $output = curl_exec($ch);
+}
