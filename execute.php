39e961f4a5c1cadab54a6183e64cac1f50dfa90f
kerlux <kerlux@gmail.com>
2016-10-28 11:44:40 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -15,7 +15,7 @@ $text = strtolower($text);
 if(strpos($text, 'pizz') !== false)
 {
   $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
-  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("1.jpg")), 'caption' => $text);
+  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("1.jpg")), 'caption' => '1');
   $ch = curl_init(); 
   curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
   curl_setopt($ch, CURLOPT_URL, $botUrl); 
@@ -24,7 +24,7 @@ if(strpos($text, 'pizz') !== false)
   // read curl response
   $output = curl_exec($ch);
 
-  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("2.jpg")), 'caption' => $text);
+  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("2.jpg")), 'caption' => '2');
   $ch = curl_init(); 
   curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
   curl_setopt($ch, CURLOPT_URL, $botUrl); 
@@ -33,7 +33,7 @@ if(strpos($text, 'pizz') !== false)
   // read curl response
   $output = curl_exec($ch);
 
-  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("3.jpg")), 'caption' => $text);
+  $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("3.jpg")), 'caption' => '3');
   $ch = curl_init(); 
   curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
   curl_setopt($ch, CURLOPT_URL, $botUrl); 
